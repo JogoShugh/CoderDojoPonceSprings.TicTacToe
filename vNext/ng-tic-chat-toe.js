6c902e82fb281913633dfabc8f536eedb94de00b
jogoshugh <jgough@JGough.corp.versionone.net>
2013-09-17 00:11:30 -0400

    Major improvements to support multiple games in a single page, along with an in-memory bus stand-in in place of PubNub for testing.

--- vNext/ng-tic-chat-toe.js
+++ vNext/ng-tic-chat-toe.js
@@ -1,101 +1,142 @@
 (function() {
   'use strict';
-
+  function invoke(scope, obj, functionName, args) {
+    var handler = obj[functionName];
+    if (handler && typeof handler == 'function') {
+      var phase = scope.$root.$$phase;
+      if(phase == '$apply' || phase == '$digest') {
+        handler.apply(obj, args);
+      } 
+      else {
+        scope.$apply(function() {
+          handler.apply(obj, args);
+        });          
+      }
+    }
+  }
+        
   var app = angular.module('tic-chat-toe', ['ui.bootstrap']);
 
-  app.controller('lobbyCtrl', function($scope, $rootScope) {
-    var lobby_channel_name = 'The_Lobby';
-    /*  
-      Lobby functions:
-      The_Lobby is a channel that we use
-      to send message about overall things,
-      such as games getting created.
-    */
-    function subscribe_to_The_Lobby() {
-      var subscription_setup = {
-        channel: lobby_channel_name,
-        callback: function(message) {
-          $scope.$apply(function() {
-            var handler_function_name = 'on' + message.message_type;
-            if ($scope[handler_function_name]) {
-              $scope[handler_function_name](message);
-            }
-          });
-        }
-      };
-      PUBNUB.subscribe(subscription_setup);
+  app.service('BusInMemory', function() {
+    var channels = [];
+    function channelFind(name) {
+      return _.findWhere(channels, {name: name});
     }
 
-    function send_lobby_message(message_type, data) {
-      var message_to_send = {
-        channel: lobby_channel_name,
+    function Bus() {
+      // We don't really need the scope here...
+    }
+
+    Bus.prototype.subscribe = function(channel, callback) {
+      var subscription = {
+        channel: channel,
+        callback: callback
+      };
+      var channel = channelFind(subscription.channel);
+      if (!channel) {
+        channel = { name: subscription.channel, subscribers:[] }
+        channels.push(channel);
+      }
+      channel.subscribers.push(subscription.callback);
+    };
+
+    Bus.prototype.publish = function(channel, messageType, data) {
+      var message = {
+        channel: channel,
         message: {
-          message_type: message_type,
-          data: data
+          message_type: messageType,
+          message: data
         }
-      };
-      PUBNUB.publish(message_to_send);
-      return false;
-    }
-
-    $rootScope.sendLobbyMessage = send_lobby_message;
-
-    function create_game_channel(gameName, game) {
-      var subscriptionSetup = {
-          channel: gameName,
-          callback: function(message) {
-            $scope.$apply(function() {
-              var handlerFunctionName = 'on' + message.message_type;
-              if (game[handlerFunctionName]) {
-                game[handlerFunctionName](message.data);
-              }
-            });
-          }
-      };
-      PUBNUB.subscribe(subscriptionSetup);
-    }
-
-    $rootScope.createGameChannel = function(gameName, game) {      
-      create_game_channel(gameName, game);
-      send_lobby_message('game_created', gameName);      
-    }
-
-    subscribe_to_The_Lobby();
+      };        
+      var channel = channelFind(message.channel);
+      if (channel) {
+        _.each(channel.subscribers, function(subscriber) {
+          subscriber(message.message);
+        });
+      }
+    };
+    
+    return new Bus();
   });
 
-  app.controller('gameCtrl', function($scope, $rootScope, $timeout) {
-    $scope.gameStarted = false;
+  app.service('Bus', function() {
+    return {
+      subscribe: function(channel, callback) {
+        var subscription = {
+          channel: channel,
+          callback: callback
+        };
+        PUBNUB.subscribe(subscription);
+      },
+      publish: function(channel, messageType, data) {
+        var message = {
+          channel: channel,
+          message: {
+            message_type: messageType,
+            message: data
+          }
+        };
+        PUBNUB.publish(message);
+      }
+    };
+  });
+
+  app.controller('lobbyCtrl', function($scope, $rootScope, Bus) {
+    //var Bus = BusInMemory;
+    var lobbyChannel = 'The_Lobby';
     $scope.boardSize = 3;
-    $scope.streakSize = 3;
+    $scope.streakSize = 3;    
+
+    function subscribeToTheLobby() {
+      Bus.subscribe(lobbyChannel, function(message) {
+        invoke($scope, $scope, 'on' + message.message_type, [message]);
+      });
+    }
+
+    function publishToLobby(messageType, data) {
+      Bus.publish(lobbyChannel, messageType, data);
+    }
+
+    var games = [];
+    $scope.games = games;
+
+    var gameName = 'Josh';
+    var gameIndex = 0;
+
+    $scope.gameCreate = function() {
+      var gameChannel = gameName + gameIndex++;
+      games.push(gameChannel);
+    }
+
+    $rootScope.createGameChannel = function(gameName, game) {
+      Bus.subscribe(gameName, function(message) {
+        invoke($scope, game, 'on' + message.message_type, [message.message]);
+      });    
+      publishToLobby('game_created', gameName);
+    };    
+
+    $rootScope.sendLobbyMessage = publishToLobby;
+
+    subscribeToTheLobby();
+  });
+
+  app.controller('gameCtrl', function($scope, $rootScope, $timeout, Bus) {
+    //var Bus = BusInMemory;
+    $scope.gameStarted = false;
     $scope.moveAttempted = false;
-    $scope.gameName = 'Josh';
+
     $scope.gameStart = function() {
       var game = new TicChatToe($scope.boardSize, $scope.streakSize);
       $scope.board = game.getBoard();
       $scope.game = game;
       $scope.gameStarted = true;
+      console.log($scope.gameName);
       $rootScope.createGameChannel($scope.gameName, game);
     };
 
     function sendGameMessage(message_type, data) {
-      var message_to_publish = {
-        channel: $scope.gameName,
-        message: {
-          message_type: message_type,
-          data: data
-        }
-      };
-      PUBNUB.publish(message_to_publish);
-      return false;
+      Bus.publish($scope.gameName, message_type, data)
     }
-    
-    /*
-    $scope.move = function(cell) {
-      var moveAttempt = TicChatToe.move(cell.row, cell.col, $scope.game.playerCurrent);
-      $scope.moveAttempted = true;
-      $scope.game.onmove(moveAttempt);
-    };
-    */
 
     $scope.move = function(cell) {
       var moveAttempt = TicChatToe.move(cell.row, cell.col, $scope.game.playerCurrent);
