20357cfb50617ed3960cd25ca4f485e4ea15f178
Josh Gough <jsgough@gmail.com>
2014-03-27 17:19:55 -0400

    - Message storage in mongo - Query for "current" peers in a mission

--- tutorial/server/server.js
+++ tutorial/server/server.js
@@ -1,9 +1,67 @@
-var io = require('socket.io').listen(8090);
+var mongo = require('mongodb').MongoClient;
 
-io.sockets.on('connection', function(socket) {
-	socket.on('msg', function(msg) {
-		console.log('here is the message from a client:')
-		console.log(msg);
-		socket.broadcast.emit("msg", msg);
-	});
+var msgs = null;
+
+function decorateMessage(msg) {
+	msg.timestamp = new Date();
+	return msg;
+}
+
+mongo.connect("mongodb://localhost:27017/learnlocity", function(err, db) {
+	msgs = db.collection('msg');
+
+	var io = require('socket.io').listen(8090);
+	io.sockets.on('connection', function(socket) {
+		socket.on('msg', function(msg) {
+			var decoratedMessage = decorateMessage(msg);
+			socket.broadcast.emit("msg", decoratedMessage);
+			msgs.insert(decoratedMessage, function(err, result) {
+				if (err) {
+					console.log('Error:');
+					console.log(err);
+				}
+			});
+		});
+		socket.on('query', function(query) {
+			console.log('query is:');
+			console.log(query);
+			 msgs.find(query).toArray(function(err, items) {
+			 	if (err) {
+			 		console.log('Error:');
+			 		console.log(err);
+			 	} else {
+			 		socket.emit('queryResult', items);
+			 	}
+			 });
+		});
+		socket.on('queryMissionUsersNow', function(mission) {
+			console.log('queryMissionUsersNow is:');
+			console.log(mission);
+
+			var now = new Date();
+			var since = new Date(now - 1*60*1000);
+
+			var query = {
+				mission: mission,
+				timestamp:{	$gte:since}
+			};
+
+			msgs.find(query).toArray(function(err, items) {
+			 	if (err) {
+			 		console.log('Error:');
+			 		console.log(err);
+			 	} else {
+			 		console.log('queryMissionUsersNowResponse:');
+			 		console.log(items);
+			 		var response = {
+			 			mission: mission,
+			 			items: items
+			 		};
+			 		socket.emit('queryMissionUsersNowResponse', response);
+			 	}
+			 });
+		});		
+
+		
+	});	
 });
